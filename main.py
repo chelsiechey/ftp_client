4be7e79ee42ecc595ec9ccedd12fdbdc5ae83b57
Chelsie Conrad <chelsie.conrad@LMRM-7034624.local>
2022-07-12 19:32:24 -0600

    if no secrets file is provided, getting login credentials from user using cli

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from ftplib import FTP
-from secrets import password
+from os.path import exists
 
 host = 'localhost'
-user = 'chelsieconrad'
 
 with FTP(host) as ftp:
+    if exists('secrets.py'):
+        from secrets import password, user
+    else:
+        print('Please enter your username')
+        user = input()
+        print('Please enter your password')
+        password = input()
 
-    ftp.login(user=user, passwd=password)
-
-    # Confirm connection
-    print(ftp.getwelcome())
+    ftp_response = ftp.login(user=user, passwd=password)
+    print(ftp_response)
 
     # Upload test_upload_file.txt to the FTP server as upload.txt
     with open('test_upload_file.txt', 'rb') as f:
-        ftp.storbinary('STOR ' + 'upload.txt', f)
+        ftp_response = ftp.storbinary('STOR ' + 'upload.txt', f)
+        print(ftp_response)
 
     # Retrieve upload.txt from the FTP server and write it to test.txt
     with open('test.txt', 'wb') as f:
-        ftp.retrbinary('RETR ' + 'upload.txt', f.write, 1024)
+        ftp_response = ftp.retrbinary('RETR ' + 'upload.txt', f.write, 1024)
+        print(ftp_response)
+
+    # Delete a file
+    ftp_response = ftp.delete('upload.txt')
+    print(ftp_response)
 
     ftp.quit()
